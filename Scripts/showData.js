c0aa496616fdefa1d08727c440900541fb07b5b9
Laura Muñoz <lauramunozg@iesanagustin.edu.co>
2022-01-16 22:26:02 -0500

    Sprint N°1

--- Scripts/showData.js
+++ Scripts/showData.js
@@ -3,12 +3,12 @@ export const showData = async (list, element) => {
 
     productos.forEach(producto => {
         const {id, descuento, img, Precio_descuento, Precio_original, nombre, descripcion,categoria } = producto;
-        const precioIva = Precio_descuento * 0.19
+        
         element.innerHTML += `
-        <div>
+
             <div class="productosJava"  style="display:inline-table">
                 <span id="descuento"><b>${descuento}dto</b></span>
-                <img src="${img}" alt="${nombre}" id="" class="img-producto">
+                <img src="${img}" data-img="${img}" alt="${nombre}"  class="img-producto">
 
                 <div class="info">
                     <b id="Precio-descuento">$${Precio_descuento}/Kg</b>
@@ -16,15 +16,62 @@ export const showData = async (list, element) => {
                     <p id="nombre">${nombre}</p>
                 </div>
 
-                <button id="${id}" class="agregar"><a href="#openModal" id="vinculo-modal"  onchange="agregar_producto(id);">Agregar</a></button>
+                <button data-id=${id} class="agregar"><a class="" href="#openModal" id="vinculo-modal" >Agregar</a></button>
             </div>
-
-        </div>
             
         `
 
+        const cards = document.querySelector('.producto-info');
+        let carrito = {}
+
+        cards.addEventListener('click', e => {
+            addCarrito(e);
+        })
+
+
+        const addCarrito = e => {
+               // console.log(e.target);
+               // console.log(e.target.classList.contains('agg'));
+                if (e.target.classList.contains('agregar')) {
+                    setProducto(e.target.parentElement);
+                }
+                e.stopPropagation()
+        }
+
+        const setProducto = objeto => {
+           //console.log(objeto)
+            const product = {
+                id: objeto.querySelector('.agregar').dataset.id,
+                title: objeto.querySelector('#nombre').textContent,
+                precio: objeto.querySelector('#Precio-descuento').textContent,
+                img: objeto.querySelector('.img-producto').dataset.img,
+                cantidad: 1
+
+            }
+           // console.log(product)
+            
+            if (carrito.hasOwnProperty(product.id)) {
+                product.cantidad = carrito[product.id].cantidad + 1;
+            }
+
+            carrito[product.id] = { ...product}
+            pintarProducto();
+        }
+
+        const pintarProducto = () => {
+           // console.log(carrito)
+           objeto.values(carrito).forEach(item => {
+               const {title, precio, img} = item
+               const precioIva = precio * 0.19
+               
+           })
+        }
+
     });
+
 }
+
+
 
 export const showData2 = async (list, element) => {
     const productos = await list;
@@ -40,7 +87,7 @@ export const showData2 = async (list, element) => {
             </div>
             <span id="peso">${peso}</span><br><br>
 
-            <button id="" class="agregar"><a href="#openModal" id="vinculo-modal">Agregar</a></button>
+            <button data-id=${id} class="agregar"><a href="#openModal" id="vinculo-modal">Agregar</a></button>
         </div>
             
         `
